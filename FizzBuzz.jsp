c2dfab087ddeb0eba3f823494fc9b09ceb504a37
yoshikyoto <admin@h96.26.238.10.30790.vlan.kuins.net>
2014-10-20 23:33:36 +0900

    複数のinput-outputに対応しようとした
    けどうまく行かなかったので途中セーブ。

--- FizzBuzz.jsp
+++ FizzBuzz.jsp
@@ -10,13 +10,25 @@
 
 <form method="POST" action="submit">
 <h4>Question:
+
+<%@ page import="java.io.*" %>
+<% String root = "/Users/admin/junit/"; %>
+<% File file = new File(root); %>
+
 <select name="name">
-  <option value="FizzBuzz">FizzBuzz</option>
+<%
+for(File qfile : file.listFiles()){ 
+	if(!qfile.isDirectory()) continue;
+	String name = qfile.getName();
+	out.print("<option value=\"" + name + "\">" + name + "</option>");
+}
+%>
 </select>
 </h4>
 
 <h4>Input Code:</h4>
 <p><textarea name="submited_code" rows="20" cols="100"></textarea></p>
+
 <p><input type="submit" value="Submit" class="btn btn-primary"></p>
 </form>
 </body>
--- WEB-INF/src/YLabCoder/Submit.java
+++ WEB-INF/src/YLabCoder/Submit.java
@@ -3,12 +3,17 @@ package YLabCoder;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Enumeration;
 
@@ -43,7 +48,8 @@ public class Submit extends HttpServlet {
 	    
 	    // ディレクトリの準備
         String date = getDateString();
-	    File qfile = new File(root + name);
+        File qfile = new File(root + name);
+        File ofile = new File(root + name + "/output.txt");
 	    File file = new File(root + name + "/" + date);
 	    file.mkdir();
 	    
@@ -52,40 +58,114 @@ public class Submit extends HttpServlet {
 		pw.print(code);
 		pw.close();
 		
-		String actual = "Server Error";
+		/* 
+		 * 0 結果
+		 * 1 引っかかったinput
+		 * 2 引っかかった想定のoutput
+		 * 3 引っかかった実際のoutput
+		 */
+		String strs[] = new String[4];
+		
+		String result = "";
 		try{
 			// コードのコンパイル
 			Runtime rt = Runtime.getRuntime();
 			Process p = rt.exec("javac Main.java", null, file);
 			p.waitFor();
+			
+			// 入出力ファイルの取得
+			ArrayList<String> inputs = getAll("input", qfile);
+			ArrayList<String> outputs = getAll("output", qfile);
+			
+			if(inputs.size() == 0){
+				// input が無い場合
+				String expected = getStringFromFile(ofile);
+				strs = executeCode(file, expected);
+			}else{
+				// input が複数ある場合
+				for(int i = 0; i < inputs.size(); i++){
+					String input = inputs.get(i);
+					String output = outputs.get(i);
+					strs = executeCode(file, input, output);
+				}
+			}
 
-			// コードの実行
-			p = rt.exec("java Main", null, file);//("java Main", null, file);
-			InputStream is = p.getInputStream();
-			InputStreamReader isr = new InputStreamReader(is);
-			BufferedReader br = new BufferedReader(isr);
-
-			actual = "";
-			String line = "";
-			while((line = br.readLine()) != null){
-				actual += line + "\n";
+			if(strs[0].equals("AC")){
+				result = "<span class=\"label label-success\">Accepted</span>";
+			}else{
+				result = "<span class=\"label label-warning\">Wrong Answer</span>";
 			}
 		}catch(Exception e){
-			actual += "\n" + e;
+			result += "<span class=\"label label-danger\">Server Error</span>" + e;
 		}
 		
 	    out.println("<h2 class=\"page-header\">Result</h2>");
+	    out.println("<h4>" + result + "</h4>");
 	    out.println("<h4>Question: " + name + "</h4>");
 	    out.println("<h4>Code:</h4>");
 	    out.println("<pre class=\"prettyprint\">" + code + "</pre>");
 		
 	    // 実行結果
-	    out.println("<h4>Output</h4>");
-	    out.println("<pre class=\"prettyprint\">" + actual + "</pre>");
+	    out.println("<h4>Output:</h4>");
+	    out.println("<pre class=\"prettyprint\">" + strs[3] + "</pre>");
 
 	    out.println("</body>");
 	    out.println("</html>");
 	    out.close();
+	}
+	
+	public static String[] executeCode(File file, String input, String output) throws IOException{
+		String strs[] = new String[4];
+		// コードの実行
+		Runtime rt = Runtime.getRuntime();
+		Process p = rt.exec("java Main", null, file);//("java Main", null, file);
+		InputStream is = p.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		
+		OutputStream os = p.getOutputStream();
+		PrintWriter pw = new PrintWriter(os);
+		pw.println(output);
+
+		String actual = "";
+		String line = "";
+		while((line = br.readLine()) != null){
+			actual += line + "\n";
+		}
+		if(actual.equals(output)){
+			strs[0] = "AC";
+		}else{
+			strs[0] = "WA";
+		}
+		strs[1] = input;
+		strs[2] = output;
+		strs[3] = actual;
+		return strs;
+	}
+	
+	public static String[] executeCode(File file, String expected) throws IOException{
+		String strs[] = new String[4];
+		// コードの実行
+		Runtime rt = Runtime.getRuntime();
+		Process p = rt.exec("java Main", null, file);//("java Main", null, file);
+		InputStream is = p.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+
+		String actual = "";
+		String line = "";
+		while((line = br.readLine()) != null){
+			actual += line + "\n";
+		}
+		if(actual.equals(expected)){
+			strs[0] = "AC";
+		}else{
+			strs[0] = "WA";
+		}
+		strs[1] = "(no inputs)";
+		strs[2] = expected;
+		strs[3] = actual;
+		return strs;
 	}
 	
 	public static void printHeader(PrintWriter out, String title){
@@ -108,5 +188,28 @@ public class Submit extends HttpServlet {
 		BufferedWriter bw = new BufferedWriter(fw);
 		PrintWriter pw = new PrintWriter(bw);
 		return pw;
+	}
+	
+	public static ArrayList<String> getAll(String kind, File dir) throws IOException{
+		ArrayList<String> arr = new ArrayList<String>();
+		int i = 0;
+		while(true){
+			File file = new File(dir.getAbsolutePath() + "/" + kind + i + ".txt");
+			if(!file.exists()) break;
+			String str = getStringFromFile(file);
+			arr.add(str);
+		}
+		return arr;
+	}
+	
+	public static String getStringFromFile(File file) throws IOException{
+		FileReader fr = new FileReader(file);
+		BufferedReader br = new BufferedReader(fr);
+		String line, result = "";
+		while((line = br.readLine()) != null)
+			result += line + "\n";
+		br.close();
+		fr.close();
+		return result;
 	}
 }
